ea2666b1acbb394ce12346ba41a7f8b0dc4962ab
AIE- Machine Learning & Image Processing <48950631+aieml@users.noreply.github.com>
2019-12-02 10:01:38 +0530

    Update 1.0 Reading the dataset.py

--- 1.0 Reading the dataset.py
+++ 1.0 Reading the dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-dataset=pd.read_csv('heart.csv').as_matrix()
+dataset=pd.read_csv('heart.csv').values
+#this code read the whole dataset and store it in a numpy array
 data=dataset[:,0:13]
 target=dataset[:,13]
